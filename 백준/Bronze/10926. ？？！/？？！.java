8fc8268cc869830b09d4a9ec5e6522652d0733a8
Kwangchoon Lim <64191373+dev4choonii@users.noreply.github.com>
2022-09-22 04:23:27 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class Main {
+    public static void main(String[] args) throws IOException {
+        final String surfix = "??!";
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String name = br.readLine();
+        
+        System.out.println(name + surfix);
+    }
+}
