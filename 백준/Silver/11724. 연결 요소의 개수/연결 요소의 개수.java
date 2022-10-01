231b3306dfefe16a6991881f32b4038190b22c5d
Kwangchoon Lim <64191373+dev4choonii@users.noreply.github.com>
2022-10-01 14:57:43 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 732 ms, Memory: 142148 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -1,101 +1,62 @@
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
-import java.util.Currency;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    static int connectionComponentCount = 0;
-
-    static StringBuilder sb = new StringBuilder();
+    static ArrayList<Integer>[] graph;
+    static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
 
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
              BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8))) {
+            StringBuilder sb = new StringBuilder();
 
             // code
 
-            StringTokenizer stringTokenizer = new StringTokenizer(br.readLine());
-            int N = Integer.parseInt(stringTokenizer.nextToken());
-            int E = Integer.parseInt(stringTokenizer.nextToken());
+            StringTokenizer neInput = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(neInput.nextToken());
+            int E = Integer.parseInt(neInput.nextToken());
 
-            Graph graph = new Graph(N);
+            graph = new ArrayList[N+1];
+            visited = new boolean[N+1];
 
-            for (int i = 0; i < E; i++) {
-                StringTokenizer e = new StringTokenizer(br.readLine());
-                graph.addEdge(Integer.parseInt(e.nextToken()), Integer.parseInt(e.nextToken()));
+            for (int i = 1; i < N+1; i++) {
+                graph[i] = new ArrayList<>();
             }
 
-            for (int i = 1; i <= N; i++) {
-                Node node = graph.nodes[i];
-                if (!node.visited) {
-                    graph.dfs(node.data);
+            for (int i = 0; i < E; i++) {
+                StringTokenizer eInput = new StringTokenizer(br.readLine());
+                int start = Integer.parseInt(eInput.nextToken());
+                int end = Integer.parseInt(eInput.nextToken());
+
+                graph[start].add(end);
+                graph[end].add(start);
+            }
+
+            int connectionComponentCount = 0;
+            for (int i = 1; i < graph.length; i++) {
+                if (!visited[i]) {
+                    connectionComponentCount++;
+                    dfs(i);
                 }
             }
 
             sb.append(connectionComponentCount);
-
-            ////////////////////
 
             bw.write(sb.toString());
             bw.flush();
         }
     }
 
-    static class Graph {
-        Node[] nodes;
+    static void dfs(int index) {
+        if (visited[index]) return;
 
-        public Graph(int size) {
-            this.nodes = new Node[size+1];
-            for (int i = 0; i < nodes.length; i++) {
-                this.nodes[i] = new Node(i);
-            }
-        }
-
-        void addEdge(int i1, int i2) {
-            Node n1 = nodes[i1];
-            Node n2 = nodes[i2];
-
-            if (!n1.adjacentNodes.contains(n2)) {
-                n1.adjacentNodes.add(n2);
-            }
-
-            if (!n2.adjacentNodes.contains(n1)) {
-                n2.adjacentNodes.add(n1);
-            }
-        }
-
-        void dfs(int index) {
-            Stack<Node> stack = new Stack<>();
-            Node root = nodes[index];
-            stack.push(root);
-
-            while (!stack.isEmpty()) {
-                Node currentNode = stack.pop();
-                currentNode.visited = true;
-                for (Node adjacentNode : currentNode.adjacentNodes) {
-                    if (!adjacentNode.visited) {
-                        adjacentNode.visited = true;
-                        stack.push(adjacentNode);
-                    }
-                }
-            }
-
-            connectionComponentCount++;
-        }
-    }
-
-    static class Node {
-        int data;
-        ArrayList<Node> adjacentNodes = new ArrayList<>();
-        boolean visited;
-
-        Node(int data) {
-            this.data = data;
-            this.visited = false;
+        visited[index] = true;
+        for (int current : graph[index]) {
+            dfs(current);
         }
     }
 }
