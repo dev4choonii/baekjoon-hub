913de75d6c4ce3d60dfc6be51a877e961d53a78e
Kwangchoon Lim <dev4choonii@gmail.com>
2022-10-17 18:52:45 +0900

    [Bronze V] Title: N 찍기, Time: 228 ms, Memory: 18380 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,23 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8))) {
+            StringBuilder sb = new StringBuilder();
+            // code ////////////////////////////////////////////////////////////////////////////////////////
+
+            int n = Integer.parseInt(br.readLine());
+            for (int i = 1; i <= n; i++) {
+                sb.append(i).append("\n");
+            }
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////
+            bw.write(sb.toString());
+            bw.flush();
+        }
+    }
+}
