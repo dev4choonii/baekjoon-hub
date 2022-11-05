1320b9ce69f31a9598cbc9d5428b4fe8d80efa0f
Kwangchoon Lim <dev4choonii@gmail.com>
2022-11-05 22:47:10 +0900

    [Bronze V] Title: 문자열, Time: 120 ms, Memory: 13992 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out, StandardCharsets.UTF_8))) {
+            StringBuilder sb = new StringBuilder();
+            // code ////////////////////////////////////////////////////////////////////////////////////////
+
+            int t = Integer.parseInt(br.readLine());
+            for (int i = 0; i < t; i++) {
+                String word = br.readLine();
+                sb.append(word.charAt(0)).append(word.charAt(word.length()-1)).append("\n");
+            }
+
+            ////////////////////////////////////////////////////////////////////////////////////////////////
+            bw.write(sb.toString());
+            bw.flush();
+        }
+    }
+}
